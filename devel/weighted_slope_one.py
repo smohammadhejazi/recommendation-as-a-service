bbeb62b9c8dcfe62de1dbba1026db664e0e1edd0
Mohammad Hejazi <78293534+smohammadhejazi@users.noreply.github.com>
2022-09-13 11:36:52 +0430

    Predict user rating with specific querry

--- devel/weighted_slope_one.py
+++ devel/weighted_slope_one.py
@@ -1,9 +1,10 @@
+import pandas as pd
 from surprise import SlopeOne
+from surprise import SVD
 from surprise import Dataset
 from surprise import Reader
 from surprise.model_selection import cross_validate
 from user_recom import UserRecommendation
-
 
 if __name__ == "__main__":
     base_dataset_dir = "../dataset/ml-100k/"
@@ -13,7 +14,17 @@ if __name__ == "__main__":
                                ["user_id", "item_id", "rating", "timestamp"], info_sep="|", ratings_sep="\t")
 
     reader = Reader(rating_scale=(1, 5))
-    data = Dataset.load_from_df(recom_module.user_ratings[['user_id', 'item_id', 'rating']], reader)
-    algo = SlopeOne()
 
-    cross_validate(algo, data, measures=['RMSE', 'MAE'], cv=5, verbose=True)
+    ratings = recom_module.user_ratings[(recom_module.user_ratings["user_id"] == 1) &
+                                        (recom_module.user_ratings["item_id"] == 1)]
+    print(ratings)
+
+    data = Dataset.load_from_df(recom_module.user_ratings[['user_id', 'item_id', 'rating']], reader)
+    train_set = data.build_full_trainset()
+
+    algo = SlopeOne()
+    algo.fit(train_set)
+
+    uid = 1
+    iid = 1
+    pred = algo.predict(uid, iid, verbose=True)
