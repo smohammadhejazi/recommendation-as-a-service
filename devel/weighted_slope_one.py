74d448a4d5cda9207b95309b899af296ea6799bc
Mohammad Hejazi <78293534+smohammadhejazi@users.noreply.github.com>
2022-09-20 20:06:29 +0430

    Delete excessive lines

--- devel/weighted_slope_one.py
+++ devel/weighted_slope_one.py
@@ -1,9 +1,6 @@
-import pandas as pd
 from surprise import SlopeOne
-from surprise import SVD
 from surprise import Dataset
 from surprise import Reader
-from surprise.model_selection import cross_validate
 from user_recom import UserRecommendation
 
 if __name__ == "__main__":
@@ -14,10 +11,6 @@ if __name__ == "__main__":
                                ["user_id", "item_id", "rating", "timestamp"], info_sep="|", ratings_sep="\t")
 
     reader = Reader(rating_scale=(1, 5))
-
-    ratings = recom_module.user_ratings[(recom_module.user_ratings["user_id"] == 1) &
-                                        (recom_module.user_ratings["item_id"] == 1)]
-    print(ratings)
 
     data = Dataset.load_from_df(recom_module.user_ratings[['user_id', 'item_id', 'rating']], reader)
     train_set = data.build_full_trainset()
