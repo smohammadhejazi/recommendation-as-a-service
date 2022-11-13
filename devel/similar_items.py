a679edfd4c88afdfa152709e4ebe74ba537d5c4f
Mohammad Hejazi <78293534+smohammadhejazi@users.noreply.github.com>
2022-11-13 17:26:35 +0330

    rewrite similar_items with new funtions added

--- devel/similar_items.py
+++ devel/similar_items.py
@@ -4,12 +4,12 @@ from surprise import Dataset, KNNBasic
 
 def name_to_id(name, items):
     movie = items[items["movie_title"] == name]
-    return movie["movie_id"]
+    return movie["movie_id"].item()
 
 
 def id_to_name(iid, items):
     movie = items[items["movie_id"] == iid]
-    return movie["movie_name"]
+    return movie["movie_name"].item()
 
 
 if __name__ == "__main__":
@@ -25,23 +25,6 @@ if __name__ == "__main__":
                                 "thriller", "war", "western"],
                                info_sep="|", ratings_sep="\t", item_sep="|")
 
-    print(name_to_id("Dead Man Walking (1995)", recom_module.item_info))
-    exit(0)
-
-    def read_item_names():
-
-        file_name = base_dataset_dir + "u.item"
-        rid_to_name = {}
-        name_to_rid = {}
-        with open(file_name, encoding="ISO-8859-1") as f:
-            for line in f:
-                line = line.split("|")
-                rid_to_name[line[0]] = line[1]
-                name_to_rid[line[1]] = line[0]
-
-        return rid_to_name, name_to_rid
-
-
     # First, train the algorithm to compute the similarities between items
     data = Dataset.load_builtin("ml-100k")
     trainset = data.build_full_trainset()
@@ -49,23 +32,23 @@ if __name__ == "__main__":
     algo = KNNBasic(sim_options=sim_options)
     algo.fit(trainset)
 
-    # Read the mappings raw id <-> movie name
-    rid_to_name, name_to_rid = read_item_names()
+    # Get id of a movie
+    toy_story_raw_id = name_to_id("Toy Story (1995)", recom_module.item_info)
 
-    # Retrieve inner id of the movie Toy Story
-    toy_story_raw_id = name_to_rid["Toy Story (1995)"]
+    # Convert into inner id of the train set
     toy_story_inner_id = algo.trainset.to_inner_iid(toy_story_raw_id)
 
-    # Retrieve inner ids of the nearest neighbors of Toy Story.
+    # Get the inner ids of the closest 10 movies
     toy_story_neighbors = algo.get_neighbors(toy_story_inner_id, k=10)
 
-    # Convert inner ids of the neighbors into names.
+    # Convert inner ids to real ids
     toy_story_neighbors = (
         algo.trainset.to_raw_iid(inner_id) for inner_id in toy_story_neighbors
     )
-    toy_story_neighbors = (rid_to_name[rid] for rid in toy_story_neighbors)
+    # Convert real ids to names
+    toy_story_neighbors = (id_to_name(rid, recom_module.item_info) for rid in toy_story_neighbors)
 
-    print()
-    print("The 10 nearest neighbors of Toy Story are:")
+    # Print them
+    print("The nearest 10 movies are:")
     for movie in toy_story_neighbors:
         print(movie)
