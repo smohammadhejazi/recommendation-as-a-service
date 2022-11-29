806af7f7707af09be25d6473a33f622052a63b0d
Mohammad Hejazi <78293534+smohammadhejazi@users.noreply.github.com>
2022-11-29 17:10:03 +0330

    Write cold start module

--- src/modules/cold_start.py
+++ src/modules/cold_start.py
@@ -2,8 +2,13 @@ from .module_base import ModuleBase
 
 
 class ColdStart(ModuleBase):
-    def __init__(self):
-        ModuleBase.__init__(self)
+    def __init__(self, dataset, options={}):
+        ModuleBase.__init__(self, dataset, options)
+        self.popular_table = None
 
-    def recommend(self):
-        pass
+    def fit(self):
+        self.popular_table = self.dataset.groupby(["item_id"])["rating"].sum().reset_index(). \
+            sort_values(by=["rating", "item_id"], ascending=[False, False])
+
+    def recommend(self, n=10):
+        return self.popular_table.head(n)
