c6288fb8d9d3a889206a99f413b0a56ab2eb4254
Mohammad Hejazi <78293534+smohammadhejazi@users.noreply.github.com>
2022-12-06 17:38:52 +0330

    Update constructor

--- src/modules/module_base.py
+++ src/modules/module_base.py
@@ -2,3 +2,5 @@ class ModuleBase:
     def __init__(self, user_rating, options):
         self.user_rating = user_rating
         self.options = options
+        self.verbose = self.options.get("verbose", False)
+        self.is_fit = False
