646ea34869fd949555326a58da6e298f3db389be
Mohammad Hejazi <78293534+smohammadhejazi@users.noreply.github.com>
2022-12-06 13:39:44 +0330

    Add SimilarItems and UserSpecific

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,4 @@
 from .recommendation_service import RecommendationService
 from .modules.cold_start import ColdStart
+from .modules.similar_items import SimilarItems
+from .modules.user_specific import UserSpecific
