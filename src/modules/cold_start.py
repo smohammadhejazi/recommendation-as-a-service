ef4dc95d3824f889d85d3f6a60bdd00f2ea5c42d
Mohammad Hejazi <78293534+smohammadhejazi@users.noreply.github.com>
2022-12-06 13:40:23 +0330

    Change the __init__

--- src/modules/cold_start.py
+++ src/modules/cold_start.py
@@ -2,12 +2,14 @@ from .module_base import ModuleBase
 
 
 class ColdStart(ModuleBase):
-    def __init__(self, dataset, options={}):
-        ModuleBase.__init__(self, dataset, options)
+    def __init__(self, user_rating, options=None):
+        if options is None:
+            options = {}
+        ModuleBase.__init__(self, user_rating, options)
         self.popular_table = None
 
     def fit(self):
-        self.popular_table = self.dataset.groupby(["item_id"])["rating"].sum().reset_index(). \
+        self.popular_table = self.user_rating.groupby(["item_id"])["rating"].sum().reset_index(). \
             sort_values(by=["rating", "item_id"], ascending=[False, False])
 
     def recommend(self, n=10):
