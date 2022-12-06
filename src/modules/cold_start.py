281a0f25a92e4d61bb2fea337c11911d1c9ae4bd
Mohammad Hejazi <78293534+smohammadhejazi@users.noreply.github.com>
2022-12-06 17:38:28 +0330

    Complete ColdStart module

--- src/modules/cold_start.py
+++ src/modules/cold_start.py
@@ -9,8 +9,15 @@ class ColdStart(ModuleBase):
         self.popular_table = None
 
     def fit(self):
+        if self.verbose:
+            print("Ranking items...")
         self.popular_table = self.user_rating.groupby(["item_id"])["rating"].sum().reset_index(). \
             sort_values(by=["rating", "item_id"], ascending=[False, False])
+        self.is_fit = True
+        if self.verbose:
+            print("All items are ranked.")
 
     def recommend(self, n=10):
+        if self.is_fit is False:
+            raise ValueError("Algorithm is not fit.")
         return self.popular_table.head(n)
