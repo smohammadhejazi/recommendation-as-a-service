a0c5bb78dc7932fd58252a8faf85605a3f870f3b
Mohammad Hejazi <78293534+smohammadhejazi@users.noreply.github.com>
2022-12-06 23:21:13 +0330

    Edit setup

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='kabirrec',
-    version='1.0.0',
+    version='1.0.1',
     description='A recommendation system with cold start, similar items and user specific recommendation',
     long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     url='https://github.com/smohammadhejazi/recommendation-as-a-service',
     author='Seyyed Mohammad Hejazi Hoseini',
     author_email='smohammadhejazi78@gmail.com',
@@ -18,20 +19,14 @@ setup(
     keywords=['recommendation-service', 'recommendation-system', 'recommender-system'],
     packages=find_packages(exclude=["devel"]),
     install_requires=[
-        "Cython>=0.29.32",
-        "flake8>=6.0.0",
         "joblib>=1.1.0",
         "kmodes>=0.12.1",
         "matplotlib>=3.5.2",
         "matrix_factorization>=1.3",
         "numpy>=1.22.3",
         "pandas>=1.4.2",
-        "pytest>=7.2.0",
         "scikit_learn>=1.1.3",
         "scipy>=1.8.0",
-        "setuptools>=58.1.0",
         "six>=1.16.0",
-        "sphinx_rtd_theme>=1.1.1",
-        "tabulate>=0.9.0",
     ]
 )
