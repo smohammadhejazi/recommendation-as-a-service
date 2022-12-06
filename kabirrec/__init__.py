6bb909d876520076ec40957d4b1de62e12fbed7c
Mohammad Hejazi <78293534+smohammadhejazi@users.noreply.github.com>
2022-12-06 23:01:06 +0330

    Edit __init__

--- kabirrec/__init__.py
+++ kabirrec/__init__.py
@@ -1,5 +1,1 @@
 from .recommendation_service import RecommendationService
-from .modules.cold_start import ColdStart
-from .modules.similar_items import SimilarItems
-from .modules.user_specific import UserSpecific
-from .utils import *
