d213d1d705637fa52c97b4d4192a8ade7d95ed2c
Mohammad Hejazi <78293534+smohammadhejazi@users.noreply.github.com>
2022-12-07 19:03:10 +0330

    Update setup

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ setup(
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
 
-    version='1.0.5',
+    version='1.0.6',
     url='https://github.com/smohammadhejazi/recommendation-as-a-service',
 
     license='MIT',
