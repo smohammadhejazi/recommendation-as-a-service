9038715c707fff0428917398f03d7d9081add86f
Mohammad Hejazi <78293534+smohammadhejazi@users.noreply.github.com>
2022-12-17 20:29:50 +0330

    Minor edit

--- api_server/server.py
+++ api_server/server.py
@@ -19,8 +19,12 @@ app = Flask(__name__)
 
 @app.route('/load-csv', methods=["POST"])
 def load_csv():
-    options = request.get_json()
-    path = options.get("path", DEFAULT_DATASET_PATH) + "/"
+    options = request.get_json(silent=True)
+    if options is None:
+        path = DEFAULT_DATASET_PATH
+    else:
+        path = options.get("path") + "/"
+
     recommendation_service.read_csv_data(
         user_info_path=path + "u.user",
         user_ratings_path=path + "u.data",
@@ -39,8 +43,11 @@ def load_csv():
 @app.route('/start-coldstart', methods=["POST"])
 def start_cold_start():
     global cold_start
-    options = request.get_json()
-    verbose = options.get("verbose", False)
+    options = request.get_json(silent=True)
+    if options is None:
+        verbose = False
+    else:
+        verbose = options.get("verbose")
     cold_start = recommendation_service.cold_start_module(options={"verbose": verbose})
     cold_start.fit()
     return json.dumps({"message": "ColdStart module is ready for use"}), 200
@@ -48,7 +55,7 @@ def start_cold_start():
 
 @app.route('/coldstart', methods=["POST"])
 def cold_start():
-    options = request.get_json()
+    options = request.get_json(silent=True)
     n = int(options.get("n", 10))
     items = cold_start.recommend(n)
     return json.dumps({"items_list": items})
@@ -57,8 +64,11 @@ def cold_start():
 @app.route('/start-similaritems', methods=["POST"])
 def start_similar_items():
     global similar_items
-    options = request.get_json()
-    verbose = options.get("verbose", False)
+    options = request.get_json(silent=True)
+    if options is None:
+        verbose = False
+    else:
+        verbose = options.get("verbose")
     similar_items = recommendation_service.similar_items_module(options={"verbose": verbose})
     similar_items.fit()
     return json.dumps({"message": "SimilarItems module is ready for use"}), 200
@@ -66,8 +76,10 @@ def start_similar_items():
 
 @app.route('/similaritems', methods=["POST"])
 def similar_items():
-    options = request.get_json()
-    item_name = options.get("name")
+    options = request.get_json(silent=True)
+    if options is None:
+        return json.dumps({"message": "item_name is required"})
+    item_name = options.get("item_name")
     n = int(options.get("n", 10))
     items = similar_items.recommend(item_name, n)
     return json.dumps({"items_list": items})
@@ -76,10 +88,16 @@ def similar_items():
 @app.route('/start-userspecific', methods=["POST"])
 def start_user_specific():
     global user_specific
-    options = request.get_json()
-    verbose = options.get("verbose", False)
-    k = int(options.get("k", None))
-    top_n = int(options.get("top_n", None))
+    options = request.get_json(silent=True)
+    if options is None:
+        verbose = False
+        k = None
+        top_n = None
+    else:
+        verbose = options.get("verbose", False)
+        k = int(options.get("k", None))
+        top_n = int(options.get("top_n", None))
+
     user_specific = recommendation_service.user_specific_module(options={"verbose": verbose, "k": k, "top_n": top_n})
     user_specific.fit()
     return json.dumps({"message": "UserSpecific module is ready for use"}), 200
@@ -87,14 +105,16 @@ def start_user_specific():
 
 @app.route('/userspecific', methods=["POST"])
 def user_specific():
-    options = request.get_json()
+    options = request.get_json(silent=True)
+    if options is None:
+        return json.dumps({"message": "item_name is required"})
     userid = options.get("userid")
-    n = int(options.get("n", None))
+    n = int(options.get("n", 10))
     items = user_specific.recommend(userid, n)
     return json.dumps({"items_list": items})
 
 
 if __name__ == "__main__":
     # TODO change port later to 80
-    # app.run(host='0.0.0.0', port=80)
+    # app.run(host='0.0.0.0', port=8080)
     app.run(host='localhost', port=7878)
