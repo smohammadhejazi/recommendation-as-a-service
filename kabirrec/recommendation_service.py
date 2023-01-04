78f56bb7810efd3ff78663e0197e0affcd481413
Mohammad Hejazi <78293534+smohammadhejazi@users.noreply.github.com>
2023-01-04 19:33:12 +0330

    Edit typos

--- kabirrec/recommendation_service.py
+++ kabirrec/recommendation_service.py
@@ -104,13 +104,13 @@ if __name__ == "__main__":
         user_info_path="../dataset/ml-100k/u.user",
         user_ratings_path="../dataset/ml-100k/u.data",
         item_info_path="../dataset/ml-100k/u.item",
-        info_columns=["user_id", "age", "gender", "occupation", "zip_code"],
-        ratings_columns=["user_id", "item_id", "rating", "timestamp"],
+        user_info_columns=["user_id", "age", "gender", "occupation", "zip_code"],
+        user_ratings_columns=["user_id", "item_id", "rating", "timestamp"],
         item_columns=["movie_id", "movie_title", "release_date", "video_release_date", "imdb_url", "unknown",
                       "action", "adventure", "animation", "children's", "comedy", "crime", "documentary",
                       "drama", "fantasy", "film_noir", "horror", "musical", "mystery", "romance", "sci-fi",
                       "thriller", "war", "western"],
-        info_sep="|", ratings_sep="\t", item_sep="|"
+        user_info_sep="|", user_ratings_sep="\t", item_sep="|"
     )
 
     cold_start = recommendation_service.cold_start_module(options={"verbose": True})
