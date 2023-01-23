b3133146bf65961712e9bdaca567d649b9b91bf4
Mohammad Hejazi <78293534+smohammadhejazi@users.noreply.github.com>
2023-01-23 13:22:12 +0330

    Update examples

--- examples/similar_item.py
+++ examples/similar_item.py
@@ -4,10 +4,11 @@ from kabirrec import RecommendationService
 if __name__ == "__main__":
     recommendation_service = RecommendationService()
 
+    data_set = "ml-100k"
     recommendation_service.read_csv_data(
-        user_info_path="../dataset/ml-100k/u.user",
-        user_ratings_path="../dataset/ml-100k/u.data",
-        item_info_path="../dataset/ml-100k/u.item",
+        user_info_path="../dataset/{}/u.user".format(data_set),
+        user_ratings_path="../dataset/{}/u.data".format(data_set),
+        item_info_path="../dataset/{}/u.item".format(data_set),
         user_info_columns=["user_id", "age", "gender", "occupation", "zip_code"],
         user_ratings_columns=["user_id", "item_id", "rating", "timestamp"],
         item_columns=["movie_id", "movie_title", "release_date", "video_release_date", "imdb_url", "unknown",
--- examples/user_specific.py
+++ examples/user_specific.py
@@ -4,10 +4,11 @@ from kabirrec import RecommendationService
 if __name__ == "__main__":
     recommendation_service = RecommendationService()
 
+    data_set = "ml-100k"
     recommendation_service.read_csv_data(
-        user_info_path="../dataset/ml-100k/u.user",
-        user_ratings_path="../dataset/ml-100k/u.data",
-        item_info_path="../dataset/ml-100k/u.item",
+        user_info_path="../dataset/{}/u.user".format(data_set),
+        user_ratings_path="../dataset/{}/u.data".format(data_set),
+        item_info_path="../dataset/{}/u.item".format(data_set),
         user_info_columns=["user_id", "age", "gender", "occupation", "zip_code"],
         user_ratings_columns=["user_id", "item_id", "rating", "timestamp"],
         item_columns=["movie_id", "movie_title", "release_date", "video_release_date", "imdb_url", "unknown",
@@ -18,9 +19,10 @@ if __name__ == "__main__":
     )
 
     user_specific = recommendation_service.user_specific_module(options={"verbose": True,
-                                                                         "k": None,
-                                                                         "top_n": 10})
-    user_specific.fit(20, 30)
+                                                                         "k": 26,
+                                                                         "k_start": 20,
+                                                                         "k_end": 30})
+    user_specific.fit()
     user_specific.draw_clusters_graph("../examples_output/user_specific_plot.png")
     prediction_rating = user_specific.recommend(1, 4)
     print(prediction_rating)
